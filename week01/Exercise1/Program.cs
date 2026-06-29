bfe3685e8197c8701af036cce2b3a374c20c22de
j4z3-83 <jaldridge@byupathway.edu>
2026-06-29 09:41:54 -0600

    assignment_1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string FName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string LName = Console.ReadLine();
+
+
     }
 }
