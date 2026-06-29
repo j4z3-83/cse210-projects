e2ec959858208273156be3a3aa6957dc8016ae24
j4z3-83 <jaldridge@byupathway.edu>
2026-06-29 11:57:29 -0600

    wk1Assignment3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string PlayAgain = "yes";
+        while (PlayAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int Number = randomGenerator.Next(1, 100);
+            int Guess;
+            int GuessCount = 0;
+            string Attempts;
+            do
+            {
+                Console.Write("What is your guess? ");
+                string GuessNum = Console.ReadLine();
+                Guess = int.Parse(GuessNum);
+                GuessCount++;
+                if(Guess < Number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (Guess > Number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    if (GuessCount == 1)
+                    {
+                        Attempts = "Guess";
+                    }
+                    else
+                    {
+                        Attempts = "Guesses";
+                    }
+
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {GuessCount} {Attempts}.");
+                }
+            } while (Guess != Number);
+            Console.Write("Would you like to play again? ");
+            PlayAgain = Console.ReadLine();
+        }
     }
 }
