455155752a8593d1f9d8150a02e2fd2ebeac2174
j4z3-83 <jaldridge@byupathway.edu>
2026-07-06 15:35:20 -0600

    wk02

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Job
+{
+    //member variables
+    public string _company = "";
+    public String _jobTitle = "";
+    public int _startYear = 0;
+    public int _endYear = 0;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
